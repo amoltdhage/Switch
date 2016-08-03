494af71a3a2cbc06f2f93403116ba6ce87cb5355
Student P_02 <studentp_02@student-p-02.local>
2016-08-03 14:16:11 +0530

    Initial Commit

---
+++ ADSwitchTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADSwitchTask
+//
+//  Created by Student P_02 on 03/08/16.
+//  Copyright © 2016 Amol Dhage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
